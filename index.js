6258d29c5236a11254155426ef0d92bcc94cdebd
CVincent88 <come.vincent@live.fr>
2020-05-11 21:00:31 -0400

    JS: liste de produits

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-// Envoi des données sur la page principale //
-// -----------------------------------------//
-let unorderedList = document.getElementById('elt-unordered-list');
-
-
+// Appel du fichier json //
+// --------------------- //
 
 let requestURL = "http://localhost:3000/api/cameras";
 let myRequest = new XMLHttpRequest();
 
+myRequest.open('GET', requestURL);
+myRequest.send();
+
+
+// Création liste item //
+// --------------------//
+let unorderedList = document.getElementById('define-list');
 
 myRequest.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response = JSON.parse(this.responseText);
-        console.log(response.length);
         for(let i = 0; i < response.length; i++ ){
+            // création élément de liste
             let newListElt = document.createElement('li');
             newListElt.classList.add('list-element');
             unorderedList.appendChild(newListElt);
+
+            // + une div à l'intérieur
+            let textContainer = document.createElement('div');
+            textContainer.classList.add('list-element_text');
+            newListElt.appendChild(textContainer);
+
+            // Ajout image produit
+            let img = document.createElement('img');
+            img.classList.add('list-element_img');
+            img.setAttribute('src', response[i].imageUrl)
+            newListElt.appendChild(img);
+
+            // Ajout nom produit
+            let name = document.createElement('span');
+            name.classList.add('list-element_text_name');
+            name.textContent = response[i].name;
+            textContainer.appendChild(name);
+
+            // Ajout description produit
+            let description = document.createElement('p');
+            description.classList.add('list-element_text_description');
+            description.textContent = response[i].description;
+            textContainer.appendChild(description);
+
+
+            // Ajout prix produit
+            let price = document.createElement('span');
+            price.classList.add('list-element_price');
+            price.textContent = '$ ' + response[i].price;
+            newListElt.appendChild(price);
         }
     }
 };
 
-myRequest.open('GET', requestURL);
-myRequest.send()
-
-
-
+// Création image dans chaque élément de liste //
+// ------------------------------------------- //
 
 
