9f50bc9dea70929f1d74160308d1101c11bad37c
CVincent88 <come.vincent@live.fr>
2020-05-15 17:21:32 -0400

    Ajustements mise en page index.html + création product.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // *** Récupération des données *** //
 // -------------------------------- //
-function obtenirInfo(url) {
+function obtenirData(url) {
     return new Promise((resolve, reject) => {
         const myRequest = new XMLHttpRequest();
         myRequest.open("GET", url);
@@ -10,58 +10,69 @@ function obtenirInfo(url) {
     });
 }
 
+
+
 // *** Construction du HTML *** //
 // ---------------------------- //
-obtenirInfo("http://localhost:3000/api/cameras")
-    .then((value) => {
+obtenirData("http://localhost:3000/api/cameras")
+    .then((cameras) => {
 
         // Récupération de l'élément HTML parent
         let defineList = document.getElementById('define-list');
 
         // Début de la boucle pour associer chaque objet à un contentant HTML
-        for(let i = 0; i < value.length; i++){
+        for(let i = 0; i < cameras.length; i++){
             // création des <li> et assignation propriétés
             let newListElt = document.createElement('li');
             newListElt.classList.add('list-element');
-            newListElt.setAttribute('id', 'list-element' + value[i]);
+            newListElt.setAttribute('data-id', `${cameras[i]._id}`);
             defineList.appendChild(newListElt);
 
-            // + une div à l'intérieur pour le texte
-            let textContainer = document.createElement('div');
-            textContainer.classList.add('list-element_text');
-            newListElt.appendChild(textContainer);
+            // Création du lien vers la page produit
+            let newLinkElt = document.createElement('a');
+            newLinkElt.setAttribute('href', `product.html?id=${cameras[i]._id}`);
+            newLinkElt.classList.add('list-element_link');
+            newListElt.appendChild(newLinkElt);
 
-            // Ajout image produit + container
+            // Contenant pour le texte
+            let textContainer = document.createElement('div');
+            textContainer.classList.add('list-element_link_text');
+            newLinkElt.appendChild(textContainer);
+
+            // Contenant pour la photo du produit
             let imgContainer = document.createElement('div');
-            imgContainer.classList.add('list-element_img');
-            newListElt.appendChild(imgContainer);
-            // +
+            imgContainer.classList.add('list-element_link_img');
+            newLinkElt.appendChild(imgContainer);
+           
+            // Photo du produit
             let img = document.createElement('img');
-            img.setAttribute('src', value[i].imageUrl);
+            img.setAttribute('src', cameras[i].imageUrl);
             imgContainer.appendChild(img);
 
-            // Ajout nom produit
+            // Nom du produit
             let name = document.createElement('span');
-            name.classList.add('list-element_text_name');
-            name.textContent = value[i].name;
+            name.classList.add('list-element_link_text_name');
+            name.textContent = cameras[i].name;
             textContainer.appendChild(name);
 
-            // Ajout description produit
+            // Description du produit
             let description = document.createElement('p');
-            description.classList.add('list-element_text_description');
-            description.textContent = value[i].description;
+            description.classList.add('list-element_link_text_description');
+            description.textContent = cameras[i].description;
             textContainer.appendChild(description);
 
-            // Ajout prix produit
+            // Prix du produit
             let price = document.createElement('span');
-            price.classList.add('list-element_price');
-            price.textContent = '$ ' + value[i].price / 100;
-            newListElt.appendChild(price);
+            price.classList.add('list-element_link_price');
+            price.textContent = '$ ' + cameras[i].price / 100;
+            newLinkElt.appendChild(price);
         }  
     })
     .catch((error) => {
-        console.log(error("Erreur du serveur"));
+        console.log(error);
     });
+
+    
 
 
 
