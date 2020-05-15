41a2b50aa4cc25892698b74cb09e73a18d163db3
CVincent88 <come.vincent@live.fr>
2020-05-15 01:19:20 -0400

    Implémentation d'une promesse pour construction index.html

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-// Appel du fichier json //
-// --------------------- //
-let requestURL = "http://localhost:3000/api/cameras";
-let myRequest = new XMLHttpRequest();
+// *** Récupération des données *** //
+// -------------------------------- //
+function obtenirInfo(url) {
+    return new Promise((resolve, reject) => {
+        const myRequest = new XMLHttpRequest();
+        myRequest.open("GET", url);
+        myRequest.onload = () => resolve(JSON.parse(myRequest.responseText));
+        myRequest.onerror = () => reject(JSON.parse(myRequest.statusText));
+        myRequest.send();
+    });
+}
 
-myRequest.open('GET', requestURL);
-myRequest.send();
+// *** Construction du HTML *** //
+// ---------------------------- //
+obtenirInfo("http://localhost:3000/api/cameras")
+    .then((value) => {
 
+        // Récupération de l'élément HTML parent
+        let defineList = document.getElementById('define-list');
 
-// Création liste item //
-// --------------------//
-let unorderedList = document.getElementById('define-list');
-
-myRequest.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        for(let i = 0; i < response.length; i++ ){
-            // création élément de liste
+        // Début de la boucle pour associer chaque objet à un contentant HTML
+        for(let i = 0; i < value.length; i++){
+            // création des <li> et assignation propriétés
             let newListElt = document.createElement('li');
             newListElt.classList.add('list-element');
-            unorderedList.appendChild(newListElt);
+            newListElt.setAttribute('id', 'list-element' + value[i]);
+            defineList.appendChild(newListElt);
 
             // + une div à l'intérieur pour le texte
             let textContainer = document.createElement('div');
@@ -31,30 +37,47 @@ myRequest.onreadystatechange = function() {
             newListElt.appendChild(imgContainer);
             // +
             let img = document.createElement('img');
-            // img.classList.add('list-element_img');
-            img.setAttribute('src', response[i].imageUrl);
+            img.setAttribute('src', value[i].imageUrl);
             imgContainer.appendChild(img);
 
             // Ajout nom produit
             let name = document.createElement('span');
             name.classList.add('list-element_text_name');
-            name.textContent = response[i].name;
+            name.textContent = value[i].name;
             textContainer.appendChild(name);
 
             // Ajout description produit
             let description = document.createElement('p');
             description.classList.add('list-element_text_description');
-            description.textContent = response[i].description;
+            description.textContent = value[i].description;
             textContainer.appendChild(description);
-
 
             // Ajout prix produit
             let price = document.createElement('span');
             price.classList.add('list-element_price');
-            price.textContent = '$ ' + response[i].price / 100;
+            price.textContent = '$ ' + value[i].price / 100;
             newListElt.appendChild(price);
-        }
-    }
-};
+        }  
+    })
+    .catch((error) => {
+        console.log(error("Erreur du serveur"));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
