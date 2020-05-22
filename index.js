ab783e485a596fa56f03e2fb8233b0b64655e20d
CVincent88 <come.vincent@live.fr>
2020-05-22 17:43:27 -0400

    Modification product.js - passage en orienté objet

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// *** Récupération des données *** //
-// -------------------------------- //
-function obtenirData(url) {
-    return new Promise((resolve, reject) => {
-        const myRequest = new XMLHttpRequest();
-        myRequest.open("GET", url);
-        myRequest.onload = () => resolve(JSON.parse(myRequest.responseText));
-        myRequest.onerror = () => reject(JSON.parse(myRequest.statusText));
-        myRequest.send();
-    });
-}
-
 // *** Construction du index.html *** //
 // ---------------------------------- //
 obtenirData("http://localhost:3000/api/cameras/")
@@ -17,10 +5,12 @@ obtenirData("http://localhost:3000/api/cameras/")
 
         for(i=0; i<cameras.length; i++){
             let newArticle = new Product(cameras[i].name, cameras[i].lenses, cameras[i]._id, cameras[i].price, cameras[i].description, cameras[i].imageUrl);
-            newArticle.createElt();
-            newArticle.createImg();
-            newArticle.createText();
-            newArticle.createPrice();
+            newArticle.listElt();
+            newArticle.image();
+            newArticle.textContainer();
+            newArticle.productName();
+            newArticle.productDescription();
+            newArticle.productPrice();
         }
     })
     .catch((error) => {
--- product.js
+++ product.js
@@ -1,77 +1,31 @@
-// *** Récupération des données *** //
-// -------------------------------- //
-function obtenirData(url) {
-    return new Promise((resolve, reject) => {
-        const myRequest = new XMLHttpRequest();
-        myRequest.open("GET", url);
-        myRequest.onload = () => resolve(JSON.parse(myRequest.responseText));
-        myRequest.onerror = () => reject(JSON.parse(myRequest.statusText));
-        myRequest.send();
-    });
-}
+// Récupération de l'Id produit
+let regex = /id=(.*)/g;
+let url = window.location.href.match(regex)
+let idProduct = url[0].substring(3);
+
 
 // *** Construction du product.html *** //
 // ------------------------------------ //
-obtenirData("http://localhost:3000/api/cameras/") // Appel de la promesse
-    .then((cameras) => {
-        for(i=0; i<cameras.length; i++){    // Parcours des données
-            if(window.location.href.includes(cameras[i]._id)){ // Si l'url de la page chargée match un objet, alors:
-                
-                //Récupération du contenant
-                let productWrapper = document.getElementById('product-wrapper');
+obtenirData("http://localhost:3000/api/cameras/" + idProduct) // Appel de la promesse
+    .then((article) => {
+        
+        let articleSelected = new Product(article.name, article.lenses, article._id, article.price, article.description, article.imageUrl);
 
-                // Création + hiérarchisation des éléments dans le contenant
+        articleSelected.image();
+        articleSelected.customizeProduct(article)
+        articleSelected.productName();
+        articleSelected.productDescription();
+        articleSelected.productPrice();
 
-                // Image produit
-                let img = document.createElement('img');
-                img.setAttribute('src', `${cameras[i].imageUrl}`);
-                img.classList.add('product-container_img');
-                productWrapper.prepend(img);
-                
-                // Personalisation du produit
-                let customizationWrapper = document.getElementById("customization-wrapper");
-                let customizationText = document.getElementById('customization-text');
-                let customizationOptionsContainer = document.getElementById("customization-options");
-                for(j=0; j<cameras[i].lenses.length; j++){
-                    let customizationOptionNumber = cameras[i].lenses.length;
+        // Ajout du produit dans le panier (localStorage) grâce au bouton
+        let addCart = document.getElementById('add-cart');
 
-                    if(customizationOptionNumber <= 1){ // Si une seule lentille disponible, message différent
-                        customizationText.textContent = "Nous n'avons que cette lentille pour le moment: "
-                    }else{
-                        customizationText.textContent = "Choisissez une lentille: "
-                    }
-                    // Options disponibles
-                    let customizationOption = document.createElement('option')
-                    customizationOption.innerText = cameras[i].lenses[j];
-                    customizationOptionsContainer.appendChild(customizationOption);
-                }
+        addCart.addEventListener('click', function(){
+            localStorage.setItem(`${article.name}`, `${article._id}`);
+            alert('Le produit a bien été ajouté au panier');
+            location.reload();
 
-                // Nom produit
-                let name = document.createElement('span');
-                name.classList.add('product-container_text_name');
-                name.innerText = cameras[i].name;
-                customizationWrapper.insertAdjacentElement('afterend', name);
-                
-                // Description produit
-                let description = document.createElement('p');
-                description.classList.add('product-container_text_description');
-                description.innerText = cameras[i].description;
-                name.insertAdjacentElement('afterend', description);
-
-                // Prix produit
-                let price = document.getElementById('product-price');
-                price.textContent = `$ ${cameras[i].price / 100}`;
+        });
             
-                // Ajout du produit dans le localStorage grâce au bouton
-                let idCam = cameras[i]._id;
-                let nomCam = cameras[i].name;
-                let addCart = document.getElementById('add-cart');
-
-                addCart.addEventListener('click', function(){
-                    localStorage.setItem(`${nomCam}`, `${idCam}`);
-                    alert('Le produit a bien été ajouté au panier');
-                    location.reload();
-                });
-            }
-        }
+        
     });
